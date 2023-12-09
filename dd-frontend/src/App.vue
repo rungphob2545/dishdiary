f57fb653cf26514604e8c0b85b2acb90d54bed1f
rungphob <rungphob.2545@mail.kmut.ac.th>
2023-12-09 14:51:45 +0700

    Add v1 Navbar

--- dd-frontend/src/App.vue
+++ dd-frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import Home from './components/Home.vue';
+import Home from "./components/Home.vue";
+import Navbar from "./components/Navbar.vue";
+import Search from "./components/Search.vue";
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import Home from './components/Home.vue';
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" /> -->
+
   <Home />
 </template>
 
---
+++ dd-frontend/src/components/Search.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "Search",
+};
+</script>
+
+<template>
+  <!-- Search input -->
+  <div class="mb-4">
+    <!-- Add w-812 and h-57 classes for width and height -->
+    <input
+      type="text"
+      class="w-[400px] h-8 bg-gray-700 text-white px-4 py-2 rounded"
+      placeholder="Search..."
+    />
+  </div>
+
+  <!-- Content -->
+</template>
+
+<style scoped>
+/* Add your additional styling here */
+</style>
